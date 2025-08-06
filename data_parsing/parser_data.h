9a9d121090dd98ef78404409306975afb732b108
David <sha082@theleys.net>
2025-08-06 13:40:18 +0100

    dfghjkjhgfdsdfghjhgfdsa

--- data_parsing/parser_data.h
+++ data_parsing/parser_data.h
@@ -4,25 +4,25 @@
 #include <QObject>
 #include <QVariant>
 #include <QDebug>
+class mei_parser; // forward declaration to resolve circular dependency
+#include "xml_parser.h"
 
 class parser_data : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QVariantList reh_y_coords READ reh_y_coords WRITE setReh_y_coords NOTIFY reh_y_coordsChanged FINAL)
-    Q_PROPERTY(QVariantList break_list READ break_list WRITE setBreak_list NOTIFY break_listChanged FINAL)
 public:
-    explicit parser_data(QObject *parent = nullptr);
+    explicit parser_data(QObject *parent = nullptr, xml_parser *xml_parser = nullptr, mei_parser *mei_parser = nullptr);
 
     void parse();
-    Q_INVOKABLE int update_break_list(QString id, QString input); // 0 = no error, -1 = invalid input,
+    int update_break_list(QString id, QString input); // 0 = no error, -1 = invalid input,
                                                                   // -2 = out of range, -3 = already exists
 
-    Q_INVOKABLE int delete_break_item(QString id); // 0 = no error, -1 = could not find break id
+    int delete_break_item(int measure_number); // 0 = no error, -1 = could not find break id
 
-    Q_INVOKABLE int new_break_item(QString input);  // 0 = no error, -1 = invalid input,
+    int new_break_item(QString input);  // 0 = no error, -1 = invalid input,
                                                     // -2 = out of range, -3 = already exists
 
-    Q_INVOKABLE void apply_breaks(); // Applies the break action list
+    void apply_breaks(); // Applies the break action list
 
     int getNumber_of_measures() const;
     QVariantList reh_y_coords() const;
@@ -37,6 +37,10 @@ signals:
     void break_listChanged();
 
 private:
+
+    xml_parser* m_xml_parser;
+    mei_parser* m_mei_parser;
+
     QVariantList m_reh_y_coords;
 
     QVariantList m_break_list;
